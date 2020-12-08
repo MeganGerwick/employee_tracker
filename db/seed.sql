becf2b78c0e7745e8c25407a82a6345872c2e78d
Louisa May <mgerwick@gmail.com>
2020-12-08 15:08:20 -0600

    added connection query to generate role list

--- db/seed.sql
+++ db/seed.sql
@@ -6,17 +6,17 @@ VALUES
 ('Manufacturing')
 ;
 
-INSERT INTO role (title, salary)
+INSERT INTO role (title, salary, department)
 VALUES 
-('Sales Lead', 60000),
-('Sales person', 40000),
-('Accountant', 70000),
-('Engineer', 80000),
-('Software Developer', 80000),
-('Customer service rep', 40000)
+('Sales Lead', 60000, 'Sales'),
+('Sales person', 40000, 'Sales'),
+('Accountant', 70000, 'Finance'),
+('Engineer', 80000, 'Manufacturing'),
+('Software Developer', 80000, 'Manufacturing'),
+('Customer service rep', 40000, 'Customer Service')
 ;
 
-INSERT INTO employee (id, first_name, last_name, role)
+INSERT INTO employee (employee_id, first_name, last_name, role)
 VALUES
 (23456, 'Samantha', 'Smith', 'Engineer'),
 (30487, 'Amir', 'Cole', 'Sales Lead'),
--- index.js
+++ index.js
@@ -3,70 +3,105 @@ const inquirer = require('inquirer');
 const consoletable = require('console.table');
 const asciiartlogo = require('asciiart-logo');
 const prompts = require('./prompts.js');
-const connection = require('./functions');
-
-
-// console.log(connection);
+const funct = require('./functions');
+const connection = require('./db/connection');
 
 
 prompts.startup()
     .then(data => {
         console.log(data);
         if (data.startup === 'View departments') {
-            connection.viewDepartment()
+            funct.viewDepartment()
                 .then(data => {
-                    console.log(data);
+                    console.table(data);
                 })
                 .catch(error => {
                     console.log(error)
                 });
             ;
-
-            prompts.startup();
         };
+
+
         if (data.startup === 'View roles') {
-            connection.viewRoles()
+            funct.viewRoles()
                 .then(data => {
-                    console.log(data);
+                    console.table(data);
                 })
                 .catch(error => {
                     console.log(error)
                 });
             ;
-
-            prompts.startup();
         };
         if (data.startup === 'View employees') {
-            connection.viewEmployees()
+            funct.viewEmployees()
                 .then(data => {
-                    console.log(data);
+                    console.table(data);
                 })
                 .catch(error => {
                     console.log(error)
                 });
-            ;
-        };
-        prompts.startup();
-        if (data.startup === 'Add department') {
-            prompts.addDepartment();
-            //INSERT INTO department FUNCTION
-            //display roles function
             prompts.startup();
-        };
-        if (data.startup === 'Add role') {
-            prompts.addRole().then(answers => {
-                console.log(answers);
-                //INSERT INTO role FUNCTION
-                //display roles function
-                prompts.startup();
-            })
 
         };
+
+        if (data.startup === 'Add department') {
+            prompts.addDepartment()
+                .then(function (answer) {
+                    connection.query(
+                        'INSERT INTO department SET ?',
+                        {
+                            name: answer.name,
+                        },
+                        function (err) {
+                            if (err) throw err;
+                            console.log('Department added!');
+                            prompts.startup();
+                        }
+                    );
+
+                });
+            ;
+        };
+
+        if (data.startup === 'Add role') {
+            prompts.addRole()
+                .then(function (answer) {
+                    connection.query(
+                        'INSERT INTO role SET ?',
+                        {
+                            title: answer.name,
+                            salary: answer.salary,
+                            department: answer.department,
+                        },
+                        function (err) {
+                            if (err) throw err;
+                            console.log('Role added!');
+                            prompts.startup();
+                        }
+                    );
+
+                });
+            ;
+        };
         if (data.startup === 'Add employee') {
-            prompts.addEmployee();
-            //INSERT INTO employee FUNCTION
-            //display employee function
-            prompts.startup();
+            prompts.addEmployee()
+                .then(function (answer) {
+                    connection.query(
+                        'INSERT INTO employee SET ?',
+                        {
+                            first_name: answer.firstname,
+                            last_name: answer.lastname,
+                            role: answer.role,
+                        },
+                        function (err) {
+                            if (err) throw err;
+                            console.log('Role added!');
+                            prompts.startup();
+                        }
+                    );
+
+                });
+            ;
         };
         if (data.startup === 'Update employee roles') {
             prompts.updateRole();
@@ -76,5 +111,6 @@ prompts.startup()
         };
     });
 
+;
 
 
